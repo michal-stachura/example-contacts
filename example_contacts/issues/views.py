d057d9197d417837e361380eed97dfdfd9496eaa
Michal Stachura <michal.stachura@santri.pl>
2023-04-25 18:33:46 +0200

    1. Add thanyou page. 2. Add minlength validator. 3. Modify users menu

--- example_contacts/issues/views.py
+++ example_contacts/issues/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from example_contacts.issues.forms import ContactForm
 
 def contact_form(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            # Redirect to thank you page
-
+            return redirect("issues:thank_you")
+            
     else:
         form = ContactForm()
 
-    return render(request, 'contact_form.html', {'form': form})
+    return render(request, "contact_form.html", {"form": form})
+
+
+def thankyou_page(request):
+    return render(request, "thank_you_page.html")
